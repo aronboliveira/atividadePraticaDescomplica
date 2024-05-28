94281189c688b8fe2f02f77537a68563ca823375
Aron-PC <aron.b.96@gmail.com>
2024-05-28 17:27:28 -0300

    Versão com imagem

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+document.querySelectorAll("img").forEach((img) => {
+  const newSpan = Object.assign(document.createElement("span"), {
+    id: `caption-${img.src.slice(img.src.lastIndexOf("/") + 1)}`.replace(
+      ".jpeg",
+      ""
+    ),
+    innerText: `*imagem gerada pelo DallE©`,
+  });
+  newSpan.classList.add(`caption-img`);
+  img.insertAdjacentElement("afterend", newSpan);
+});
+
 document.querySelectorAll("script").forEach((script) => {
   /function/gi.test(script.textContent) && script.remove();
 });
